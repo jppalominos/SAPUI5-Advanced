3c9309996ff4fef1cd281234c63200da647e900f
jppalominos <juan.palominos@gmail.com>
2021-07-21 19:28:58 +0000

    Validacion de Datos

--- Employees/webapp/controller/Main.controller.js
+++ Employees/webapp/controller/Main.controller.js
@@ -146,6 +146,9 @@ sap.ui.define([
                         tableIncidence.removeAllContent();
 
                         for (var incidence in data.results) {
+
+                            data.results[incidence]._ValidateDate = true;
+                            
                             var newIncidence = sap.ui.xmlfragment("logaligroup.Employees.fragment.NewIncidence", this._detailEmployeeView.getController());
                             this._detailEmployeeView.addDependent(newIncidence);
                             newIncidence.bindElement("incidenceModel>/" + incidence);
