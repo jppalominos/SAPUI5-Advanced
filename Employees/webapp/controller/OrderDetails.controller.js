deb7f3f12f2f8e96a4e0089094bde67d171d3fde
jppalominos <juan.palominos@gmail.com>
2021-07-23 01:24:40 +0000

    Historial de Navegacion

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -1,18 +1,33 @@
 // @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.core.routing.History} History 
      */
-    function (Controller) {
+    function (Controller, History) {
         'use strict';
 
         return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
 
             onInit: function () {
 
+            },
+
+            onBack: function (oEvent) {
+
+                var oHistory = History.getInstance();
+                var sPreviusHash = oHistory.getPreviousHash();
+
+                if (sPreviusHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("RouteMain", true);
+                };
             }
         });
     });
